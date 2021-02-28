9e17040f877dc6958776f660c9a32883d0657bab
rynz01 <78792626+rynz01@users.noreply.github.com>
2021-02-28 20:59:04 +0700

    Update aja

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
+  this.sendContact(m.chat, '6283856085455', 'Rynz 01', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
