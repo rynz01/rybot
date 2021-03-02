e6afe13e5f857aa72c87176b364236969d592342
rynz01 <78792626+rynz01@users.noreply.github.com>
2021-03-03 06:13:47 +0700

    Update cheat.js

--- plugins/cheat.js
+++ plugins/cheat.js
@@ -6,6 +6,6 @@ selamat anda mendapatkan exp sebanyak ${handler.exp}
 `.trim()) // Tambah sendiri kalo mau
 
 handler.exp = 5555555555
-handler.command = /^dona(te|si)$/i
+handler.command = /^cheat$/i
 handler.owner = true
 module.exports = handler
