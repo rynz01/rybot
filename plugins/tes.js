7e5569f5e7c1dbe4208463fcb5b172b0c89639fc
rynz01 <78792626+rynz01@users.noreply.github.com>
2021-03-04 06:03:14 +0700

    Create tes.js

---
+++ plugins/tes.js
@@ -0,0 +1,16 @@
+const { MessageType } = require('@adiwajshing/baileys')
+.sendFile(m.chat, '../src/sticker.webp', "", null, m, false, { asSticker: true })
+
+handler.command = /^anjay$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
