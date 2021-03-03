f2c68f0f7332f35722fb590a57e420ddea51edac
rynz01 <78792626+rynz01@users.noreply.github.com>
2021-03-03 18:48:59 +0700

    Update oadd.js

--- plugins/oadd.js
+++ plugins/oadd.js
@@ -2,16 +2,16 @@ let handler = async (m, { conn, text }) => {
   let users = text.split`,`.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v.length > 20)
   conn.groupAdd(m.chat, users)
 }
-handler.help = ['add', '+'].map(v => 'o' + v + ' @user')
-handler.tags = ['owner']
-handler.command = /^(oadd|o\+)$/i
+handler.help = ['add 6285××××××')
+handler.tags = ['admin']
+handler.command = /^(add)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = true
 handler.private = false
 
-handler.admin = false
+handler.admin = true
 handler.botAdmin = true
 
 handler.fail = null
