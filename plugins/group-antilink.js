326da6dff673c7b0c2a7ff8bb8d58ae56527e7bd
rynz01 <78792626+rynz01@users.noreply.github.com>
2021-03-03 08:15:19 +0700

    Add files via upload

---
+++ plugins/group-antilink.js
@@ -0,0 +1,31 @@
+let { Presence } = require('@adiwajshing/baileys')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+	if(!args || !args[0]) {
+		await conn.updatePresence(m.chat, Presence.composing) 
+		conn.reply(m.chat, `*Format salah! Contoh :*\n\n	*○ ${usedPrefix + command} on*\n	*○ ${usedPrefix + command} off*`, m)
+	} else if(args[0] == 'on') {
+		let cek = global.DATABASE._data.chats[m.chat].nolink
+	if(cek) return conn.reply(m.chat, `*Anti-Link telah aktif pada grup ini.*`, m)
+		await conn.updatePresence(m.chat, Presence.composing) 
+		global.DATABASE._data.chats[m.chat].nolink = true
+		conn.reply(m.chat, `*Anti-Link berhasil diaktifkan.*`, m)
+	} else if(args[0] == 'off') {
+		let cek = global.DATABASE._data.chats[m.chat].nolink
+	if(!cek) return conn.reply(m.chat, `*Anti-Link telah di nonaktifkan pada grup ini.*`, m)
+		await conn.updatePresence(m.chat, Presence.composing) 
+		global.DATABASE._data.chats[m.chat].nolink = false
+		conn.reply(m.chat, `*Anti-Link berhasil di nonaktifkan.*`, m)
+	} else {
+		await conn.updatePresence(m.chat, Presence.composing) 
+		conn.reply(m.chat, `*Format salah! Contoh :*\n\n	*○ ${usedPrefix + command} on*\n	*○ ${usedPrefix + command} off*`, m)
+	} 
+}
+handler.help = ['antilink']
+handler.tags = ['']
+handler.command = /^(antilink)$/i
+handler.owner = true
+handler.admin = false
+handler.botAdmin = true
+handler.exp = 0
+handler.limit = false
+module.exports = handler
