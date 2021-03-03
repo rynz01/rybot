b06c0ff6566e58a06fa9b39a6b85d6e6015d12e6
rynz01 <78792626+rynz01@users.noreply.github.com>
2021-03-03 08:07:57 +0700

    Update afk

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ let rl = Readline.createInterface(process.stdin, process.stdout)
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = ['6281515860089'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.owner = ['6283856085455'] // Put your number here
+global.mods = ['62856085455'] // Want some help?
+global.prems = ['62856085455'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
