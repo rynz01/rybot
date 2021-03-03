9162f9bd58a5a37dade30d247a1e470a47802c9e
rynz01 <78792626+rynz01@users.noreply.github.com>
2021-03-03 20:11:59 +0700

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, participants }) => {
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].isBanned = true
-    m.reply('Done!')
+    m.reply('*Sekarang public dimatikan di group ini*')
   // } else m.reply('Ada nomor host disini...')
 }
-handler.help = ['banchat']
+handler.help = ['public off']
 handler.tags = ['owner']
-handler.command = /^banchat$/i
+handler.command = /^public off$/i
 handler.owner = true
 
 module.exports = handler
