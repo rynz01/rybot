3aab43c257e766eff2907d24cf34c25853cbca12
rynz01 <78792626+rynz01@users.noreply.github.com>
2021-03-03 14:01:41 +0700

    Update cheat.js

--- plugins/cheat.js
+++ plugins/cheat.js
@@ -1,12 +1,36 @@
-let handler = async m => m.reply(`
-conn.reply(m.chat, `*Jawaban Benar!*\n+9999 XP`, m)
-    global.DATABASE._data.users[m.sender].exp += 9999
-
-
-
-`.trim()) // Tambah sendiri kalo mau
-
-
-handler.command = /^cheat$/i
+let handler = async (m, { conn }) => {
+  if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
+    global.DATABASE._data.users[m.sender].exp += 999999
+    m.reply('Selamat anda mendapatkan 999999 Exp')
+    global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
+  } else m.reply('Udah nge cheat masih aja mau nge cheat lagi😒')
+}
+handler.help = ['cheat]
+handler.tags = ['owner']
+handler.command = /^(cheat)$/i
 handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+
 module.exports = handler
+
+
+
+
+
+
+
+
+
+
+
+
+
